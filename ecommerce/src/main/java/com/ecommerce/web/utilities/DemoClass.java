9b5370a4fdf228d41d50a63063d20d38f7832857
shivaramakrishna <shivasrk1994@gmail.com>
2018-09-21 17:07:37 +0530

    Second commit

---
+++ ecommerce/src/main/java/com/ecommerce/web/utilities/DemoClass.java
@@ -0,0 +1,24 @@
+package com.ecommerce.web.utilities;
+
+/**
+ * @author shiva rama
+ *
+ */
+public class DemoClass {
+
+	
+	
+	
+	
+	
+ public void jstMethod() {
+	 
+	 
+	 System.out.println("this is just a method");
+	 System.out.println("this is not  just a method");
+	 
+		
+	}
+	
+	
+}
